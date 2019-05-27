6bf76d61d323c6f47fee0ad5b8a5db5038780d27
Ken MacFarlane <macfarlane.ken@gmail.com>
2019-05-27 19:57:26 -0400

    remove separate json config in favor of keyword arguments

--- __init__.py
+++ __init__.py
@@ -5,15 +5,15 @@ import requests
 
 
 def get_settings_from_json():
-    return json.load(open('/home/ksm/lastfm-revive/settings.json', "r"))
+    return json.load(open('settings.json', "r"))
 
 
-def get_lastfm_total_artist_tracks(artist, settings):
+def get_lastfm_total_artist_tracks(artist, username, api_key):
     params = {
         'artist': artist,
         'autocorrect': 1,
-        'username': settings['username'],
-        'api_key': settings['api_key'],
+        'username': username,
+        'api_key': api_key,
         'format': 'json'
     }
     r = requests.get('http://ws.audioscrobbler.com/2.0/?method=artist.getInfo', params=params)
@@ -25,17 +25,10 @@ def get_lastfm_total_artist_tracks(artist, settings):
         return r.json().get('artist').get('stats').get('userplaycount')
     except AttributeError:
         return 0
-    #
-    # print(json.dumps(r.json(), indent=2))
-    # return 0
 
 
-def get_artist_tracks(artist):
-    settings = get_settings_from_json()
-    lastfm_total_artist_tracks = get_lastfm_total_artist_tracks(artist, settings)
+def get_artist_tracks(artist, username=None, api_key=None, use_cache=True, cache_directory='lastfm-cache'):
+    if username is None or api_key is None:
+        raise ValueError("username and api_key are required parameters")
+    lastfm_total_artist_tracks = get_lastfm_total_artist_tracks(artist, username, api_key)
     return "hello world " + str(lastfm_total_artist_tracks)
-
-
-# if __name__ == "__main__":
-#     tracks = get_artist_tracks('Foo Fighters')
-#     print(tracks)
