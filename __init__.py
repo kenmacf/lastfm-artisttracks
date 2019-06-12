7109cdc3e293627107d022f0f0aa4ce309c0847e
Ken MacFarlane <macfarlane.ken@gmail.com>
2019-06-11 22:44:47 -0400

    error codes, kwargs name change

--- __init__.py
+++ __init__.py
@@ -18,17 +18,19 @@ def get_lastfm_total_artist_tracks(artist, username, api_key):
     }
     r = requests.get('http://ws.audioscrobbler.com/2.0/?method=artist.getInfo', params=params)
     if r.status_code != requests.codes.ok:
-        return 0
+        return -1
     if r.json().get('error', 0) > 0:
-        return 0
+        return -2
     try:
-        return r.json().get('artist').get('stats').get('userplaycount')
+        user_play_count = r.json().get('artist').get('stats').get('userplaycount')
+        # artist_mbid = r.json().get('artist')
+        return user_play_count
     except AttributeError:
-        return 0
+        return -3
 
 
-def get_artist_tracks(artist, username=None, api_key=None, use_cache=True, cache_directory='lastfm-cache'):
+def get_artist_tracks(artist, username=None, api_key=None, cache_method='file', cache_location='lastfm-cache'):
     if username is None or api_key is None:
         raise ValueError("username and api_key are required parameters")
     lastfm_total_artist_tracks = get_lastfm_total_artist_tracks(artist, username, api_key)
-    return "hello world " + str(lastfm_total_artist_tracks)
+    return "artist {} tracks {}".format(artist, lastfm_total_artist_tracks)
